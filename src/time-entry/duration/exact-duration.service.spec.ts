84756555e928ac9a2b802b78af44270fa18fccc0
Enrico Battistella <enrico.battistella@digitalrockers.it>
2026-01-07 14:46:54 +0100

    duration srv

---
+++ src/time-entry/duration/exact-duration.service.spec.ts
@@ -0,0 +1,10 @@
+import { ExactDurationService } from "./exact-duration.service"
+
+describe('ExactDurationService', () => {
+  let srv: ExactDurationService;
+
+  beforeEach(() => {
+    srv = new ExactDurationService();
+  })
+
+});
